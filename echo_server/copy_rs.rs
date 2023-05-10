9255616ba7143b90de3583e1b633875b6e93a404
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2023-05-10 16:21:41 +1000

    WIP port of libsharedringbuffer to Rust

---
+++ echo_server/copy_rs.rs
@@ -0,0 +1,152 @@
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+
+mod shared_ringbuffer;
+
+use sel4cp::{debug_println, main, NullHandler};
+use shared_ringbuffer::{ring_init, RingHandle};
+
+#[main(heap_size = 0x10000)]
+fn main() -> NullHandler {
+    debug_println!("Hello, World!");
+
+    /* Set up shared memory regions */
+    // ring_init(&rx_ring_mux, (ring_buffer_t *)rx_free_mux, (ring_buffer_t *)rx_used_mux, NULL, 1);
+    // ring_init(&rx_ring_cli, (ring_buffer_t *)rx_free_cli, (ring_buffer_t *)rx_used_cli, NULL, 0);
+
+    /* Enqueue free buffers for the mux to access */
+    for i in 0..NUM_BUFFERS {
+        let _ = shared_dma_vaddr_mux + (BUF_SIZE * i);
+        // let err = enqueue_free(&rx_ring_mux, addr, BUF_SIZE, NULL);
+        // assert(!err);
+    }
+
+    NullHandler::new()
+}
+
+static rx_free_mux: usize = 0;
+static rx_used_mux: usize = 0;
+
+static rx_free_cli: usize = 0;
+static rx_used_cli: usize = 0;
+
+static shared_dma_vaddr_mux: usize = 0;
+static shared_dma_vaddr_cli: usize = 0;
+static uart_base: usize = 0;
+
+const MUX_RX_CH: usize = 0;
+const CLIENT_CH: usize = 1;
+
+const BUF_SIZE: usize = 2048;
+const NUM_BUFFERS: usize = 512;
+const SHARED_DMA_SIZE: usize = BUF_SIZE * NUM_BUFFERS;
+
+static mut initialised: bool = false;
+
+// static rx_ring_mux: *mut RingHandle = {};
+// static rx_ring_cli: *mut RingHandle = {};
+
+// void process_rx_complete(void)
+// {
+//     bool mux_was_full = ring_full(rx_ring_mux.used_ring);
+//     uint64_t mux_free_original_size = ring_size(rx_ring_mux.free_ring);
+//     bool cli_used_was_empty = ring_empty(rx_ring_cli.used_ring);
+//     uint64_t enqueued = 0;
+//     // We only want to copy buffers if all the dequeues and enqueues will be successful
+//     while (!ring_empty(rx_ring_mux.used_ring) &&
+//             !ring_empty(rx_ring_cli.free_ring) &&
+//             !ring_full(rx_ring_mux.free_ring) &&
+//             !ring_full(rx_ring_cli.used_ring)) {
+//         uintptr_t m_addr, c_addr = 0;
+//         unsigned int m_len, c_len = 0;
+//         void *cookie = NULL;
+//         void *cookie2 = NULL;
+//         int err;
+
+//         err = dequeue_used(&rx_ring_mux, &m_addr, &m_len, &cookie);
+//         assert(!err);
+//         // get a free one from clients queue.
+//         err = dequeue_free(&rx_ring_cli, &c_addr, &c_len, &cookie2);
+//         assert(!err);
+//         if (!c_addr ||
+//                 c_addr < shared_dma_vaddr_cli ||
+//                 c_addr >= shared_dma_vaddr_cli + SHARED_DMA_SIZE)
+//         {
+//             print("COPY|ERROR: Received an insane address: ");
+//             puthex64(c_addr);
+//             print(". Address should be between ");
+//             puthex64(shared_dma_vaddr_cli);
+//             print(" and ");
+//             puthex64(shared_dma_vaddr_cli + SHARED_DMA_SIZE);
+//             print("\n");
+//         }
+
+//         if (c_len < m_len) {
+//             print("COPY|ERROR: client buffer length is less than mux buffer length.\n");
+//             print("client length: ");
+//             puthex64(c_len);
+//             print(" mux length: ");
+//             puthex64(m_len);
+//             print("\n");
+//         }
+//         // copy the data over to the clients address space.
+//         memcpy((void *)c_addr, (void *)m_addr, m_len);
+
+//         /* Now that we've copied the data, enqueue the buffer to the client's used ring. */
+//         err = enqueue_used(&rx_ring_cli, c_addr, m_len, cookie2);
+//         assert(!err);
+//         /* enqueue the old buffer back to dev_rx_ring.free so the driver can use it again. */
+//         err = enqueue_free(&rx_ring_mux, m_addr, BUF_SIZE, cookie);
+//         assert(!err);
+
+//         enqueued += 1;
+//     }
+
+//     if (cli_used_was_empty && enqueued) {
+//         sel4cp_notify_delayed(CLIENT_CH);
+//     }
+
+//     /* We only want to signal the mux if the free queue was
+//         empty and we enqueued something, or that the used queue was full and 
+//         we dequeued something, OR 
+//         potentially the mux pre-empted us (as it's higher prio) and emptied the queue
+//         while we were enqueuing, and thus the OG size and number of packets we 
+//         processed doesn't add up and the mux could potentially miss this 
+//         empty -> non-empty transition. */
+//     if ((mux_free_original_size == 0 || mux_was_full || 
+//             mux_free_original_size + enqueued != ring_size(rx_ring_mux.free_ring)) 
+//             && enqueued) {
+//         if (have_signal) {
+//             // We need to notify the client, but this should
+//             // happen first. 
+//             sel4cp_notify(CLIENT_CH);
+//         }
+//         sel4cp_notify_delayed(MUX_RX_CH);
+//     }
+// }
+
+// void notified(sel4cp_channel ch)
+// {
+//     if (!initialised) {
+//         /*
+//          * Propogate this down the line to ensure everyone is
+//          * initliased in correct order.
+//          */
+//         sel4cp_notify(MUX_RX_CH);
+//         initialised = 1;
+//         return;
+//     }
+
+//     if (ch == CLIENT_CH || ch == MUX_RX_CH) {
+//         /* We have one job. */
+//         process_rx_complete();
+//     } else {
+//         print("COPY|ERROR: unexpected notification from channel: ");
+//         puthex64(ch);
+//         print("\n");
+//         assert(0);
+//     }
+// }
+
---
+++ echo_server/shared_ringbuffer.rs
@@ -0,0 +1,217 @@
+/*
+ * Copyright 2023, UNSW
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+// @ivanv: figure out cookie pointer
+// @ivanv: make sure to add thread memory release barrier
+
+use core::assert;
+use core::ffi::c_void;
+
+const RING_SIZE: u32 = 512;
+
+/* Buffer descriptor */
+pub struct BuffDesc {
+    encoded_addr: usize, /* encoded dma addresses */
+    len: u32,            /* associated memory lengths */
+    cookie: *mut c_void, /* index into client side metadata */
+}
+
+/* Circular buffer containing descriptors */
+pub struct RingBuffer {
+    write_idx: u32,
+    read_idx: u32,
+    buffers: [BuffDesc; RING_SIZE as usize],
+}
+
+/* Function pointer to be used to 'notify' components on either end of the shared memory */
+type Notify = fn();
+
+/* A ring handle for enqueing/dequeuing into  */
+pub struct RingHandle {
+    free_ring: *mut RingBuffer,
+    used_ring: *mut RingBuffer,
+    /* Function to be used to signal that work is queued in the used_ring */
+    notify: Notify,
+}
+
+/**
+ * Check if the ring buffer is empty.
+ *
+ * @param ring ring buffer to check.
+ *
+ * @return true indicates the buffer is empty, false otherwise.
+ */
+pub fn ring_empty(ring: *mut RingBuffer) -> bool {
+    return ring_size(ring) == 0;
+}
+
+/**
+ * Check if the ring buffer is full
+ *
+ * @param ring ring buffer to check.
+ *
+ * @return true indicates the buffer is full, false otherwise.
+ */
+pub fn ring_full(ring: *mut RingBuffer) -> bool
+{
+    return ring_size(ring) == RING_SIZE - 1;
+}
+
+pub fn ring_size(ring: *mut RingBuffer) -> u32
+{
+    assert!((*ring).write_idx - (*ring).read_idx >= 0);
+    return (*ring).write_idx - (*ring).read_idx;
+}
+
+/**
+ * Notify the other user of changes to the shared ring buffers.
+ *
+ * @param ring the ring handle used.
+ *
+ */
+pub fn notify(ring: &RingHandle) {
+    return (ring.notify)();
+}
+
+/**
+ * Enqueue an element to a ring buffer
+ *
+ * @param ring Ring buffer to enqueue into.
+ * @param buffer address into shared memory where data is stored.
+ * @param len length of data inside the buffer above.
+ * @param cookie optional pointer to data required on dequeueing.
+ *
+ * @return -1 when ring is empty, 0 on success.
+ */
+pub fn enqueue(ring: *mut RingBuffer, buffer: usize, len: u32, cookie: *mut c_void) -> Result<(), &'static str>
+{
+    assert!(buffer != 0);
+    if ring_full(ring) {
+        return Err("Trying to enqueue onto a full ring");
+    }
+
+    let idx = ((*ring).write_idx % RING_SIZE) as usize;
+    (*ring).buffers[idx].encoded_addr = buffer;
+    (*ring).buffers[idx].len = len;
+    (*ring).buffers[idx].cookie = cookie;
+
+    // THREAD_MEMORY_RELEASE();
+    (*ring).write_idx += 1;
+
+    Ok(())
+}
+
+/**
+ * Dequeue an element to a ring buffer.
+ *
+ * @param ring Ring buffer to Dequeue from.
+ * @param buffer pointer to the address of where to store buffer address.
+ * @param len pointer to variable to store length of data dequeueing.
+ * @param cookie pointer optional pointer to data required on dequeueing.
+ *
+ * @return -1 when ring is empty, 0 on success.
+ */
+pub fn dequeue(ring: *mut RingBuffer, addr: &mut usize, len: &mut u32, cookie: *mut *mut c_void) -> Result<(), &'static str>
+{
+    if ring_empty(ring) {
+        return Err("Trying to dequeue from an empty ring");
+    }
+
+    let idx = ((*ring).read_idx % RING_SIZE) as usize;
+    assert!((*ring).buffers[idx].encoded_addr != 0);
+    *addr = (*ring).buffers[idx].encoded_addr;
+    *len = (*ring).buffers[idx].len;
+    unsafe {
+        *cookie = (*ring).buffers[idx].cookie;
+    }
+
+    // THREAD_MEMORY_RELEASE();
+    (*ring).read_idx += 1;
+
+    Ok(())
+}
+
+/**
+ * Enqueue an element into an free ring buffer.
+ * This indicates the buffer address parameter is currently free for use.
+ *
+ * @param ring Ring handle to enqueue into.
+ * @param buffer address into shared memory where data is stored.
+ * @param len length of data inside the buffer above.
+ * @param cookie optional pointer to data required on dequeueing.
+ *
+ * @return -1 when ring is full, 0 on success.
+ */
+pub fn enqueue_free(ring: &mut RingHandle, addr: usize, len: u32, cookie: *mut c_void) -> Result<(), &'static str>
+{
+    return enqueue(ring.free_ring, addr, len, cookie);
+}
+
+/**
+ * Enqueue an element into a used ring buffer.
+ * This indicates the buffer address parameter is currently in use.
+ *
+ * @param ring Ring handle to enqueue into.
+ * @param buffer address into shared memory where data is stored.
+ * @param len length of data inside the buffer above.
+ * @param cookie optional pointer to data required on dequeueing.
+ *
+ * @return -1 when ring is full, 0 on success.
+ */
+pub fn enqueue_used(ring: &mut RingHandle, addr: usize, len: u32, cookie: *mut c_void) -> Result<(), &'static str> {
+    return enqueue(ring.used_ring, addr, len, cookie);
+}
+
+/**
+ * Dequeue an element from an free ring buffer.
+ *
+ * @param ring Ring handle to dequeue from.
+ * @param buffer pointer to the address of where to store buffer address.
+ * @param len pointer to variable to store length of data dequeueing.
+ * @param cookie pointer optional pointer to data required on dequeueing.
+ *
+ * @return -1 when ring is empty, 0 on success.
+ */
+pub fn dequeue_free(ring: &mut RingHandle, addr: &mut usize, len: &mut u32, cookie: *mut *mut c_void) -> Result<(), &'static str> {
+    return dequeue(ring.free_ring, addr, len, cookie);
+}
+
+/**
+ * Dequeue an element from a used ring buffer.
+ *
+ * @param ring Ring handle to dequeue from.
+ * @param buffer pointer to the address of where to store buffer address.
+ * @param len pointer to variable to store length of data dequeueing.
+ * @param cookie pointer optional pointer to data required on dequeueing.
+ *
+ * @return -1 when ring is empty, 0 on success.
+ */
+pub fn dequeue_used(ring: &mut RingHandle, addr: &mut usize, len: &mut u32, cookie: *mut *mut c_void) -> Result<(), &'static str> {
+    return dequeue(ring.used_ring, addr, len, cookie);
+}
+
+/**
+ * Initialise the shared ring buffer.
+ *
+ * @param ring ring handle to use.
+ * @param free pointer to free ring in shared memory.
+ * @param used pointer to 'used' ring in shared memory.
+ * @param notify function pointer used to notify the other user.
+ * @param buffer_init 1 indicates the read and write indices in shared memory need to be initialised.
+ *                    0 inidicates they do not. Only one side of the shared memory regions needs to do this.
+ */
+pub fn ring_init(ring: &mut RingHandle, free: *mut RingBuffer, used: *mut RingBuffer, notify: Notify, buffer_init: bool) {
+    ring.free_ring = free;
+    ring.used_ring = used;
+    ring.notify = notify;
+
+    if buffer_init {
+        (*ring.free_ring).write_idx = 0;
+        (*ring.free_ring).read_idx = 0;
+        (*ring.used_ring).write_idx = 0;
+        (*ring.used_ring).read_idx = 0;
+    }
+}
